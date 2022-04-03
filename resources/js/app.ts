35c71194eec2881a52b047e41a82429361168ebf
marthikinz <marthikinz@gmail.com>
2022-04-03 06:56:09 -0400

    my first in yearssss

---
+++ resources/js/app.ts
@@ -0,0 +1,5 @@
+import { createApp } from 'vue'
+import Overview from './components/Overview.vue'
+import '../css/main.css'
+
+createApp(Overview).mount('body')
---
+++ webpack.mix.js
@@ -0,0 +1,2 @@
+const mix = require("laravel-mix");
+mix.ts("resources/js/app.ts", "public/js").vue({ version: 3 });
